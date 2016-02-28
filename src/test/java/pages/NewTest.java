f74e38a48049f71f92700039814048b729bbc4eb
vsubbuvelu <vsubbuvelu@gmail.com>
2016-02-28 13:06:16 +0530

    Changed NewTest java

--- src/test/java/pages/NewTest.java
+++ src/test/java/pages/NewTest.java
@@ -7,24 +7,23 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 public class NewTest {
-  @Test
-  public void f() {
-	  
-	  WebDriver driver1;
-	  RemoteWebDriver driver2;
-	  driver1.findElement(by)
-	  	FirefoxDriver driver=new FirefoxDriver();
-	  	driver.get("http://demo1.opentaps.org/");
-	  	driver.findElementById("username").clear();
-	  	driver.findElementById("username").sendKeys("DemoSalesManager");
-	  	driver.findElementById("password").clear();
-	  	driver.findElementById("password").sendKeys("crmsfa");
-	  	driver.findElementByCssSelector("input.decorativeSubmit").
-	    //driver.findElement(By.id("username")).clear();
-	    //driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
-	    //driver.findElement(By.id("password")).clear();
-	    //driver.findElement(By.id("password")).sendKeys("crmsfa");
-	    driver.findElement(By.cssSelector("input.decorativeSubmit")).click();
-	    driver.findElement(By.cssSelector("input.decorativeSubmit")).click();
-  }
+	@Test
+	public void f() {
+
+		WebDriver driver1;
+		RemoteWebDriver driver2;
+		FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://demo1.opentaps.org/");
+		driver.findElementById("username").clear();
+		driver.findElementById("username").sendKeys("DemoSalesManager");
+		driver.findElementById("password").clear();
+		driver.findElementById("password").sendKeys("crmsfa");
+	//	driver.findElementByCssSelector("input.decorativeSubmit");
+		// driver.findElement(By.id("username")).clear();
+		// driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		// driver.findElement(By.id("password")).clear();
+		// driver.findElement(By.id("password")).sendKeys("crmsfa");
+		// driver.findElement(By.cssSelector("input.decorativeSubmit")).click();
+		// driver.findElement(By.cssSelector("input.decorativeSubmit")).click();
+	}
 }
